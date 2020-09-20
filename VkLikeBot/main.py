723cabed5e5c7e4551d8d1a4f529c7c44421a241
tghns <kkrev.denis@yandex.ru>
2020-09-20 12:36:11 +0300

    Update main.py
    Removed unneccessary comments

--- VkLikeBot/main.py
+++ VkLikeBot/main.py
@@ -13,9 +13,8 @@ plt= PlotBuilder()
 
 bot.run()
 max=30
+
 while True:
-# while True вытаскиваем из очереди айдишники и проверяем каждого по 10 мин
-# Результаты сохраняем в папку с <client id>-<aim_id> (учесть, что чел 2 раза может искать 1 тян)
     request=queue.get()
     client_id=request['client_id']
     aim_id=request['aim_id']
@@ -39,23 +38,6 @@ while True:
     print('График перекрёстных')
     plt.crossed_attractives(crossed_attractives, path=path, max=max)
 
-    # Кого и как часто, отфильтрованный по времени:
-    # print("Кого и как часто по времени")
-    # likesService.remove_fans(gen='Year')
-    # likesService.fill_fans(0, 20, 20, 20, aim_domain=aim_domain, gen='Year', period_in_days=365)
-    # year_fans=likesService.get_fans_with_info(gen='Year', gender=gender)
-    #
-    # plt.fan_to_aim(fans=year_fans, max=max)
-
-    #Графики и инфу отправляем клиенту.
-    #time.sleep(1.5*60+randint(0,60))
     bot.send_respond(client_id,aim_id)
 
     likesService.close_connection()
-
-
-
-
-
-#Создать поток, который проверяет готовность результатов и отправляет его клиенту (в отдельном методе)
-
